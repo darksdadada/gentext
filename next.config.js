663b65377d1994fed674147eca6fbd1ed1e65a01
darksdadada <darksdadada@users.noreply.github.com>
2026-03-05 10:15:26 +0800

    update

--- next.config.js
+++ next.config.js
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-  // Electron 静态导出配置
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
 }
 
 module.exports = nextConfig
